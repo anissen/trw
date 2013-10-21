77d89a85f788df01e2759e272b72adf5140d8521
Anders Nissen <andnis@gmail.com>
2013-10-21 22:51:00 +0200

    Locked the door to the dungeon

--- levels/level2.js
+++ levels/level2.js
@@ -5,11 +5,17 @@ Game.Level.Level2 = function() {
 	this._playerLight = [30, 30, 30];
 
   this._hahn = null;
+  this._doorToDungeon = null;
 };
 Game.Level.Level2.extend(Game.Level);
 
 Game.Level.Level2.prototype.fromTemplate = function(map, def) {
 	Game.Level.prototype.fromTemplate.call(this, map, def);
+
+  for (var key in this.cells) {
+    var cell = this.cells[key];
+    if (cell.getId() === 'door-to-dungeon') this._doorToDungeon = cell;
+  }
 	
 	for (var key in this.beings) {
 		var being = this.beings[key];
@@ -25,7 +31,7 @@ Game.Level.Level2.prototype.fromTemplate = function(map, def) {
 	
 	this._initStory();
 	return this;
-}
+};
 
 Game.Level.Level2.prototype._initStory = function() {
 	this._addRule(function() {
@@ -41,6 +47,15 @@ Game.Level.Level2.prototype._initStory = function() {
     Game.story.addChapter("Hmm, det blev jeg ikke meget klogere af...");
     Game.story.setTask("Find en der IKKE mumler!");
     Game.storyFlags.blah = 1;
+    return true;
+  });
+
+  this._addRule(function() {
+    return this._doorToDungeon.bumpedInto();
+  }, function() {
+    Game.story.addChapter("Døren er låst. Jeg må finde en måde at komme igennem.");
+    Game.story.setTask("Find en nøgle!");
+    //Game.storyFlags.blah = 1;
     return true;
   });
 
